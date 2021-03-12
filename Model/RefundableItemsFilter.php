4837c2688a826d6fe450c240a24692c6563d635e
Alexander Wanyoike <wanyoike.alexander@gmail.com>
2021-03-12 14:24:06 +0000

    Ensure adjustments are not returns

--- Model/RefundableItemsFilter.php
+++ Model/RefundableItemsFilter.php
@@ -2,25 +2,26 @@
 namespace SnowIO\ExtendedSalesRepositories\Model;
 
 use Magento\Sales\Api\Data\CreditmemoInterface;
+use Magento\Sales\Api\Data\CreditmemoItemInterface;
 use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\Data\OrderItemInterface;
 
 /**
  * Class RefundableItemsFilter
- *
  *
  * @package SnowIO\ExtendedSalesRepositories\Model
  */
 class RefundableItemsFilter
 {
     /**
-     * @var \SnowIO\ExtendedSalesRepositories\Model\AvailableQuantityProvider
+     * @var AvailableQuantityProvider
      */
     private $availableQuantityProvider;
 
     /**
      * RefundableItemsFilter constructor.
      *
-     * @param \SnowIO\ExtendedSalesRepositories\Model\AvailableQuantityProvider $availableQuantityProvider
+     * @param AvailableQuantityProvider $availableQuantityProvider
      */
     public function __construct(
         AvailableQuantityProvider $availableQuantityProvider
@@ -31,24 +32,28 @@ class RefundableItemsFilter
     /**
      * Filter valid items to refund
      *
-     * @param \Magento\Sales\Api\Data\OrderInterface      $order
-     * @param \Magento\Sales\Api\Data\CreditmemoInterface $creditmemo
+     * @param OrderInterface $order
+     * @param CreditmemoInterface $creditmemo
+     * @param bool $hasAdjustment
      * @return array
      * @author Daniel Doyle <dd@amp.co>
      */
-    public function filter(OrderInterface $order, CreditmemoInterface $creditmemo) : array
+    public function filter(OrderInterface $order, CreditmemoInterface $creditmemo, $hasAdjustment)
     {
         $availableQuantity = $this->availableQuantityProvider->provide($order);
 
         $validItemsToRefund = [];
-        /** @var \Magento\Sales\Model\Order\Item $orderItem */
         foreach ($order->getAllItems() as $orderItem) {
             foreach ($creditmemo->getItems() as $creditmemoItem) {
-                if ($orderItem->getSku() !== $creditmemoItem->getSku()
-                    || $creditmemoItem->getQty() <= 0
-                    || $creditmemoItem->getQty() > $availableQuantity[$orderItem->getId()]
-                ) {
+                /**
+                 * If the credit memo is an adjustment
+                 * We will not do any returns thus all specified skus
+                 * in the credit memo will have a zero quantity.
+                 */
+                if ($hasAdjustment) {
                     $validItemsToRefund[$orderItem->getId()] = 0;
+                } elseif ($this->cantBeRefunded($orderItem, $creditmemoItem, $availableQuantity)) {
+                    continue;
                 } else {
                     $validItemsToRefund[$orderItem->getId()] = $creditmemoItem->getQty();
                 }
@@ -56,5 +61,22 @@ class RefundableItemsFilter
         }
 
         return $validItemsToRefund;
+    }
+
+    /**
+     * @param OrderItemInterface $orderItem
+     * @param CreditmemoItemInterface $creditmemoItem
+     * @param array $availableQuantity
+     * @return bool
+     * @author Alexander Wanyoike <amw@amp.co>
+     */
+    private function cantBeRefunded(
+        OrderItemInterface $orderItem,
+        CreditmemoItemInterface $creditmemoItem,
+        array $availableQuantity
+    ) {
+        return $orderItem->getSku() !== $creditmemoItem->getSku()
+            || $creditmemoItem->getQty() <= 0
+            || $creditmemoItem->getQty() > $availableQuantity[$orderItem->getId()];
     }
 }
